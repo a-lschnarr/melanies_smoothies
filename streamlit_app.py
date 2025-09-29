b8bb033ad4db57e5a92780b6de0425c9a2fe193b
a-lschnarr <aschnarr@holyfamily.edu>
2025-09-29 10:14:31 -0400

    Update streamlit_app.py
    Commented out line 24

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ session = cnx.session()
 
 #st.write("Your favorite fruit is:", option)
 
-session = get_active_session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
